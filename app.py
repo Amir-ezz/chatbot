49a0480705ac432a62132f8cf1477817a9434f41
Amir-ezz <139725214+Amir-ezz@users.noreply.github.com>
2024-04-25 17:33:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import nltk
+nltk.download('punkt')
+
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 import numpy
